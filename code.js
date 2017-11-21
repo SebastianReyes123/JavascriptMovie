34e65c15b9594fd42ee519ff808bfd7aafc6d982
sebastian.reyes.c <sebastian.reyes.c@outlook.com>
2017-11-21 13:58:28 +0100

    jQueryfied

--- code.js
+++ code.js
@@ -1,11 +1,31 @@
-function renderMovie(movie){
-    document.getElementById("movieTitle").innerText = movie.Title;
-    document.getElementById("description").innerText = movie.Description;    
-    document.getElementById("image").innerHTML ="<img src="+movie.Image+">"; 
+function renderMovie(movie) {
+   $("#movieTitle").text(movie.Title);
+    $("#description").text(movie.Description);
+    $("#image").html("<img src=" + movie.Image + ">");
+    // document.getElementById("image").setAttribute("src",movie.Image) //sätts in i en <img> tag ist för div. 
     let actors = "<ul>";
-    for(let i in movie.Actors){
-        actors += "<li>"+movie.Actors[i]+"</li>";
+    for (let i in movie.Actors) {
+        actors += "<li>" + movie.Actors[i] + "</li>";
     }
     actors += "</ul>";
-    document.getElementById("list").innerHTML = actors;
+    $("#list").html(actors);
 }
+function changeStarRating(grade) {
+    for (let i = 1; i <= 5; i++) {            
+        if(i<=grade)
+        $("#star" + i).addClass("filled");              
+        else
+        $("#star" + i).removeClass("filled");              
+             
+    }
+}
+// for (let i = 1; i <= 5; i++) {
+//     $("#star" + i).click(function (e) {
+//         changeStarRating(i);
+//     });
+// }
+
+$("#stars").click("span", function(e){
+    let star = $(e.target).attr("data-star-id");
+    changeStarRating(+star);
+  });
