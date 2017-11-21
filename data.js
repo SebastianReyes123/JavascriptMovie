554c86197b39fadae1960803d922c38216f34045
sebastian.reyes.c <sebastian.reyes.c@outlook.com>
2017-11-21 09:50:44 +0100

    data moved to data.js

---
+++ data.js
@@ -0,0 +1,14 @@
+let movieData = {
+Title: "Justice Leauge",
+Image: "https://images-na.ssl-images-amazon.com/images/M/MV5BNDgwNjMwNjM1OV5BMl5BanBnXkFtZTgwNjA2Njk5MzI@._V1_UX182_CR0,0,182,268_AL_.jpg",
+Description: "Fueled by his restored faith in humanity and inspired by Superman's selfless act, Bruce Wayne enlists the help of his newfound ally, Diana Prince, to face an even greater enemy.",
+Actors:[
+   "Ben Affleck",
+   "Henry Cavill",
+    "Amy Adams",
+    "Ezra Miller",
+    "Jason Momoa",
+    "Ray Fisher",
+]
+};
+{/* <img src=""> */}
